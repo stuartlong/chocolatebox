79b015a059251dc2d6dac93906195fd72fcff4d2
Stuart Long <srl51@case.edu>
2014-02-21 17:33:04 -0500

    added simple LevelGen scripts and Unity test project

---
+++ Project/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+	public Vector2 levelSize;
+	public Vector2 baseBlockSize;
+	public SpriteRenderer groundBlock;
+	private bool done = false;
+
+	public void Start () {
+		int[,] section = new SectionBuilder(levelSize, this).Build();
+		for (int i = 0; i < section.GetLength(0); i++)
+		{
+			for (int j = 0; j < section.GetLength(1); j++)
+			{
+				if (section[i,j] == (int) AssetTypeKey.GroundBlock)
+				{
+					float centerX = groundBlock.sprite.bounds.extents.x  * 2 * i;
+					float centerY = groundBlock.sprite.bounds.extents.y * 2 * j;
+					Instantiate(groundBlock, new Vector3(centerX,centerY,0), new Quaternion());
+					Debug.Log (centerX + ", " + centerY);
+				}
+			}
+		}
+	}
+	
+	public void Update () {
+
+	}
+
+	public enum AssetTypeKey 
+	{
+		GroundBlock = 1,
+		Entrance = 2,
+		Exit = 3
+	}
+}
---
+++ Project/Assets/Scripts/SectionBuilder.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SectionBuilder {
+	private Vector2 size;
+	private LevelGenerator generator;
+
+	public SectionBuilder(Vector2 sectionSize, LevelGenerator mainGenerator)
+	{
+		size = sectionSize;
+		generator = mainGenerator;
+	}
+
+	public int[,] Build()
+	{
+		int numberBlocksX = (int) (size.x / generator.groundBlock.sprite.bounds.extents.x  * 2);
+		int numberBlocksY = (int) (size.y / generator.groundBlock.sprite.bounds.extents.y * 2);
+		int[,] section = new int[numberBlocksX,numberBlocksX];
+		float centerX = 0f;
+
+		for (int i = 0; i < numberBlocksX; i++) 
+		{
+			for (int j = 0; j < numberBlocksY; j++)
+			{
+				//for now just fill the bottom of the section with blocks
+				if (j == 0)
+				{
+					section[i,j] = (int) LevelGenerator.AssetTypeKey.GroundBlock;
+				}
+			}
+		}
+
+		return section;
+	}
+}
