29f25d6d628526a13243f0b05645db7f7d516e30
Stuart Long <srl51@case.edu>
2014-03-21 18:49:36 -0400

    Add helpful labels to the level generator editor

--- Project/Assets/Editor/LevelGeneratorEditor.cs
+++ Project/Assets/Editor/LevelGeneratorEditor.cs
@@ -10,24 +10,30 @@ using UnityEditor;
 [CustomEditor(typeof(LevelGenerator))]
 public class LevelGeneratorEditor : Editor 
 {
-	private static readonly int PIXELS_PER_TAB = 20;
+	private static readonly int VERTICAL_TAB = 10;
 	private bool blockFold = false;
 	public override void OnInspectorGUI()
 	{
 		serializedObject.Update();
 		LevelGenerator generator = (LevelGenerator) target;
 
+		EditorGUILayout.BeginVertical();
+		GUILayout.Space(VERTICAL_TAB);
 		Vector2 floatLevelSize = EditorGUILayout.Vector2Field("Level Size", generator.levelSize);
 		generator.levelSize.x = (int) floatLevelSize.x;
 		generator.levelSize.y = (int) floatLevelSize.y;
+		EditorGUILayout.LabelField("The total size of the level to be generated.");
 
-		generator.openLevel = EditorGUILayout.Toggle("Open Level (No ceiling)", generator.openLevel);
+		GUILayout.Space(VERTICAL_TAB);
+		generator.openLevel = EditorGUILayout.Toggle("Open Level", generator.openLevel);
+		EditorGUILayout.LabelField("Open levels will have no walls or ceilings.");
+		GUILayout.Space(VERTICAL_TAB);
 
 		if (!generator.openLevel) 
 		{
-		Vector2 numbSections = EditorGUILayout.Vector2Field("Number of Sections", new Vector2(generator.sectionsX, generator.sectionsY));
-		generator.sectionsX = (int) numbSections.x;
-		generator.sectionsY = (int) numbSections.y;
+			Vector2 numbSections = EditorGUILayout.Vector2Field("Number of Sections", new Vector2(generator.sectionsX, generator.sectionsY));
+			generator.sectionsX = (int) numbSections.x;
+			generator.sectionsY = (int) numbSections.y;
 		}
 		else
 		{
@@ -35,13 +41,18 @@ public class LevelGeneratorEditor : Editor
 			generator.sectionsX = (int) numbSections.x;
 			generator.sectionsY = 1;
 		}
+		EditorGUILayout.LabelField("The number of sections that will comprise your level.");
+		GUILayout.Space(VERTICAL_TAB);
 
-		blockFold = EditorGUILayout.Foldout(blockFold, "Blocks");
+		blockFold = EditorGUILayout.Foldout(blockFold, "Sprites");
 		//generator.groundBlocks = (SpriteRenderer[]) EditorGUILayout.ObjectField("Ground Blocks", generator.groundBlocks, typeof(SpriteRenderer[]), true);
 		if (blockFold) 
 		{
 			EditorGUILayout.BeginHorizontal();
 			GUILayout.Space(20);
+
+			EditorGUILayout.BeginVertical();
+			EditorGUILayout.LabelField("The sprites to use for you level. These should be prefabs.");
 
 			SerializedProperty blocks = serializedObject.FindProperty("sectionGroups");
 			EditorGUI.BeginChangeCheck();
@@ -50,15 +61,26 @@ public class LevelGeneratorEditor : Editor
 			{
 				serializedObject.ApplyModifiedProperties();
 			}
+			EditorGUILayout.EndVertical();
 			EditorGUILayout.EndHorizontal();
 		}
+		else
+		{
+			EditorGUILayout.LabelField("The sprites to use for you level. These should be prefabs.");
+		}
+		GUILayout.Space(VERTICAL_TAB);
 
-		generator.groundBlock = (SpriteRenderer) EditorGUILayout.ObjectField("Ground Block", generator.groundBlock, typeof(SpriteRenderer), true);
 		generator.player = (PlayerAttachment) EditorGUILayout.ObjectField("Player", generator.player, typeof(PlayerAttachment), true);
+		EditorGUILayout.LabelField("The player object");
+		GUILayout.Space(VERTICAL_TAB);
+
 		generator.MergeChance = EditorGUILayout.IntSlider("Section Merge Chance", generator.MergeChance, 0, 100);
+		EditorGUILayout.LabelField("The chance that one or more of your sections will merge to form a single large section.");
+		GUILayout.Space(VERTICAL_TAB);
 
 		generator.customSeed = EditorGUILayout.BeginToggleGroup("Set Custom Seed", generator.customSeed);
 		generator.seed = EditorGUILayout.IntField("Seed", generator.seed);
 		EditorGUILayout.EndToggleGroup();
+		EditorGUILayout.EndVertical();
 	}
 }
