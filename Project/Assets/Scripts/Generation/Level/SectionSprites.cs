e68dd23311dc2516dbfe0b42c4013e2381727fdd
Stuart Long <srl51@case.edu>
2014-03-21 17:37:07 -0400

    added SectionSprites script

---
+++ Project/Assets/Scripts/Generation/Level/SectionSprites.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Essentially a wrapper class for a 2D array to allow users
+/// to enter in groups of textures for sections.
+/// </summary>
+[System.Serializable]
+public class SectionSprites
+{
+	public string name;
+	public SpriteRenderer[] goundBlocks;
+	public SpriteRenderer[] wallBlocks;
+}
