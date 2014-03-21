918855a9bcd4ca1836531b8f904aea2518f3fd50
James Fitzpatrick <james.fitzpatrick@case.edu>
2014-03-21 17:19:38 -0400

    Enemy Generator working for ground units

--- Project/Assets/Scripts/Generation/Enemy/EnemyGenerator.cs
+++ Project/Assets/Scripts/Generation/Enemy/EnemyGenerator.cs
@@ -22,8 +22,7 @@ public class EnemyGenerator : MonoBehaviour
     private int[,][,] master;
     private float xunitc;
     private float yunitc;
-    
-    /*
+
     void Start()
     {
         _level_generator = (LevelGenerator)this.gameObject.GetComponent("LevelGenerator");
@@ -35,7 +34,7 @@ public class EnemyGenerator : MonoBehaviour
 
 
         //0,0 is the bottom left tile of a section/level
-    */
+
         /*
          * Algorithm:
          * 
@@ -49,63 +48,72 @@ public class EnemyGenerator : MonoBehaviour
          *      Else
          *          Probe right until end of section or ground space occurs
          */
-    /*
-        int minHDistance = (int)FindMinimumHorizontalDistance();
 
-        int x = minHDistance;
+        int minHorizontalDistance = (int)FindMinimumHorizontalDistance();
+
+        int x = 3*minHorizontalDistance;
         int y = 0;
-        int lastValidColumn = 0;
-        
+        int lastValidColumn = 2*minHorizontalDistance;
 
-        foreach (int[,] section in master)
+        Debug.Log(x + " " + y + " " + lastValidColumn);
+        
+        for (int i = 0; i < _level_generator.sectionsX; i++)
         {
-            while (x < section.Length) 
+            for (int j = 0; j < _level_generator.sectionsY; j++)
             {
-                if ((int) LevelGenerator.AssetTypeKey.GroundBlock == section[x,y])
+                //Debug.Log("Section: i = " + i + ", j = " + j);
+                int[,] section = master[i, j];
+
+                Debug.Log(section.GetLength(0));
+                
+                while (x < section.GetLength(0))
                 {
-                    if (ValidSection(section, lastValidColumn, x, y))
+                    if ((int)LevelGenerator.AssetTypeKey.GroundBlock == section[x, y])
                     {
-                        //Got there!
-                        MakeEnemy(section, x, y);
-                        lastValidColumn += minHDistance/2;
-                        x = lastValidColumn + minHDistance;
-                    }
-                    else //Well, we got close guys... oh well...
-                    {
-                        lastValidColumn = x;
-                        x += minHDistance;
-                    }
-                }
-                else
-                {
-                    if (y != 0) //Maybe the ground is just lower in this column.
-                    {
-                        y--;
-                    }
-                    else //We are at rock bottom. try a new column. 
-                    {
-                        //Find next column height
-                        while (x < section.Length)
+                        if (ValidSection(section, lastValidColumn, x, y))
                         {
-                            if ((int) LevelGenerator.AssetTypeKey.GroundBlock == section[x, y])
+                            //Got there!
+                            MakeEnemy(section, x, y);
+                            lastValidColumn += minHorizontalDistance / 2;
+                            x = lastValidColumn + minHorizontalDistance;
+                        }
+                        else //Well, we got close guys... oh well...
+                        {
+                            lastValidColumn = x;
+                            x += minHorizontalDistance;
+                        }
+                    }
+                    else
+                    {
+                        if (y != 0) //Maybe the ground is just lower in this column.
+                        {
+                            y--;
+                        }
+                        else //We are at rock bottom. try a new column. 
+                        {
+                            //Find next column height
+                            while (x < section.Length)
                             {
-                                lastValidColumn = x;
-                                x += minHDistance;
-                                break;
-                            }
-                            else
-                            {
-                                x++;
+                                if ((int)LevelGenerator.AssetTypeKey.GroundBlock == section[x, y])
+                                {
+                                    lastValidColumn = x;
+                                    x += minHorizontalDistance;
+                                    break;
+                                }
+                                else
+                                {
+                                    x++;
+                                }
                             }
                         }
                     }
+                    
                 }
+                lastValidColumn = 0;
+                x = minHorizontalDistance;
+                y = 0;
             }
-            lastValidColumn = 0;
-            x = minHDistance;
-            y = 0;
         }
-
     }
 
     private float FindMinimumHorizontalDistance()
@@ -121,21 +129,34 @@ public class EnemyGenerator : MonoBehaviour
 
     private bool ValidSection(int[,] section, int startx, int endx, int maxy)
     {
+        int checkx = (startx + endx) / 2; ; 
         int y = maxy;
-        while (y >= 0)
+        while (y >= 0 && Mathf.Abs(endx - checkx) > 1)
         {
-            if ((int) LevelGenerator.AssetTypeKey.None == section[(endx + startx) / 2, maxy])
+            if ((int) LevelGenerator.AssetTypeKey.GroundBlock == section[checkx, y])
             {
-                return true;
+                checkx = (endx + checkx) / 2;
+            }
+            else
+            {
+                y--;
+                if (y < 0)
+                {
+                    return false;
+                }
             }
         }
 
-        return false;
+        return true;
     }
 
     private void MakeEnemy(int[,] section, int x, int y)
     {
         int newY = NextAvailableY(section, x,y);
+        if (newY <= 0)
+        {
+            return;
+        }
         float convertedY = newY * yunitc;
         convertedY += Enemies[0].gameObject.renderer.bounds.extents.y;
 
@@ -148,13 +169,23 @@ public class EnemyGenerator : MonoBehaviour
 
     private int NextAvailableY(int[,] section, int x, int y)
     {
-        if ((int) LevelGenerator.AssetTypeKey.None == section[x, y]){
-            return y;
+        int tempy = y + 1;
+        while (tempy < section.GetLength(1))
+        {
+            if ((int)LevelGenerator.AssetTypeKey.None == section[x, y])
+            {
+                return y;
+            }
+            else
+            {
+                y++;
+            }
         }
+        
 
         Debug.Log("Unable to find available space");
         return -1;
     }
 
-    */
+ 
 }
