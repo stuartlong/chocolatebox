26e63c40d2685c05d5c1f5b1185f65e3ae8ec211
Stuart Long <srl51@case.edu>
2014-03-21 21:39:35 -0400

    renamed groundBlocks to belowGroundBlocks

--- Project/Assets/Scripts/Generation/Level/SectionSprites.cs
+++ Project/Assets/Scripts/Generation/Level/SectionSprites.cs
@@ -14,7 +14,7 @@ public class SectionSprites
 	/// The sprites to use for ground blocks. These act as the default blocks that make up
 	/// your level. All block sprites must be the same size.
 	/// </summary>
-	public SpriteRenderer[] groundBlocks;
+	public SpriteRenderer[] belowGroundBlocks;
 
 	/// <summary>
 	/// The sprites for the blocks that the player actually walks on. If empty, ceiling blocks will default to ground blocks.
