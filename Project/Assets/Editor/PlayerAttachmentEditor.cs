f90358707101abd2cea1206d07b717f83aed2ffe
Stuart Long <srl51@case.edu>
2014-05-01 18:39:16 -0400

    Moved attachment scripts to a seperate folder. Created editors for the attachments that didn't already have them

---
+++ Project/Assets/Editor/PlayerAttachmentEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerAttachment))]
+public class PlayerAttachmentEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		PlayerAttachment player = (PlayerAttachment) target;
+		Vector2 maxSize = EditorGUILayout.Vector2Field("Maximum Size", player.maxPlayerSize);
+		if (maxSize == null) {
+			Sprite sprite = ((SpriteRenderer) player.gameObject.GetComponent(typeof(SpriteRenderer))).sprite;
+			maxSize = new Vector2(sprite.bounds.size.x, sprite.bounds.size.y);
+		}
+		
+		if (maxSize.x <= 0) {
+			Sprite sprite = ((SpriteRenderer) player.gameObject.GetComponent(typeof(SpriteRenderer))).sprite;
+			maxSize = new Vector2(sprite.bounds.size.x, maxSize.y);
+		}
+		
+		if (maxSize.y <= 0) {
+			Sprite sprite = ((SpriteRenderer) player.gameObject.GetComponent(typeof(SpriteRenderer))).sprite;
+			maxSize = new Vector2(maxSize.x, sprite.bounds.size.y);
+		}
+		player.maxPlayerSize = maxSize;
+
+		player.maxJumpDistance = EditorGUILayout.Vector2Field("Maximum Jump Distance", player.maxJumpDistance);
+	}
+}
+	
